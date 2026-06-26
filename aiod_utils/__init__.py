8351b728919dbac42aeb4db3cbf5845ee78d94d8
sea-shunned <seashunned@gmail.com>
2026-06-26 14:23:37 +0100

    Fix smoke test with dynamic version check

--- aiod_utils/__init__.py
+++ aiod_utils/__init__.py
@@ -1,3 +1,10 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("aiod-utils")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 from aiod_utils.preprocess import Preprocess, run_preprocess
 
 __all__ = ["Preprocess", "run_preprocess"]
